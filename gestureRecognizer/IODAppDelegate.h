1b7ab6c40fd78a7a471c53c510cd85d7510dfaaf
Aditya Narayan <aditya@Adityas-iMac-9.local>
2014-01-15 12:36:21 -0500

    Initial Commit

---
+++ gestureRecognizer/IODAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IODAppDelegate.h
+//  gestureRecognizer
+//
+//  Created by Aditya Narayan on 1/15/14.
+//  Copyright (c) 2014 TurnToTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IODAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
